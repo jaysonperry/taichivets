f1886b2a3ac7c2f8159505c1d3a61efdd53330d3
Jayson Perry <jayson.t.perry@gmail.com>
2013-12-06 08:45:58 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taichivets::Application.config.secret_key_base = '806e5f2d95407a99609a423f49381c93cefdd2087ae389a41ad64baaa6e90c8d5eb5e4eda162d441f14b1369c19fd8d426eb94a7517a6c2d196205665054d1b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Taichivets::Application.config.session_store :cookie_store, key: '_taichivets_session'
