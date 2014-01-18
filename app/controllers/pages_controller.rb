434ffc85b41d22c50b777c985e929e3b19a6177d
Jayson Perry <jayson.t.perry@gmail.com>
2014-01-18 15:03:19 -0800

    home page redesign adding Devise Sign in and Sign up forms above-the-fold

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+
+  # home page
+  def home
+  end
+
+  #about company and founders page
+  def about
+  end
+
+  #contact form for application & website issue reporting & general inquirery
+  def contact
+  end
+
+  # information outside the site page
+  def resources
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Taichivets::Application.routes.draw do
+
+  root 'pages#home' 
+    get 'about' => 'pages#about'
+    get 'contact' => 'pages#contact'
+    get 'resources' => 'pages#resources'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
